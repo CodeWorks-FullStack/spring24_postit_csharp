c34d0669afeb3bfe7bf5892c8c2d9f4cfe602091
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-15 12:04:26 -0600

    checking album archived before picture create

--- server/Services/PicturesService.cs
+++ server/Services/PicturesService.cs
@@ -4,14 +4,23 @@ namespace postit_csharp.Services;
 public class PicturesService
 {
   private readonly PicturesRepository _repository;
+  private readonly AlbumsService _albumsService;
 
-  public PicturesService(PicturesRepository repository)
+  public PicturesService(PicturesRepository repository, AlbumsService albumsService)
   {
     _repository = repository;
+    _albumsService = albumsService;
   }
 
   internal Picture CreatePicture(Picture pictureData)
   {
+    Album album = _albumsService.GetAlbumById(pictureData.AlbumId);
+
+    if (album.Archived)
+    {
+      throw new Exception($"{album.Title} is archived and no longer accepting new pictures");
+    }
+
     Picture picture = _repository.CreatePicture(pictureData);
     return picture;
   }
