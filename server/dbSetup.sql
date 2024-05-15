d06bb2820d689e045861487c30e1d0cf12d3abca
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-15 09:39:39 -0600

    set up table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,32 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
+CREATE TABLE
+  IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
+  ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE
+  albums (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    creatorId VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    coverImg VARCHAR(1000) NOT NULL,
+    archived BOOLEAN NOT NULL DEFAULT false,
+    category ENUM (
+      "animals",
+      "misc",
+      "aesthetics",
+      "crocs",
+      "food",
+      "games"
+    ) DEFAULT "crocs",
+    FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+  );
+
+DROP TABLE albums;
