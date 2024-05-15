5ed6f0e2403e5e13c78b95fdae17838c83e74df3
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-15 11:34:14 -0600

    added notes

--- server/Models/RepoItem.cs
+++ server/Models/RepoItem.cs
@@ -1,5 +1,6 @@
 namespace postit_csharp.Models;
-
+// T denotes that you will supply a type when inheriting this class, similar to a parameter
+// Abstract means that this class cannot be instantiated on its own, only inherited from
 public abstract class RepoItem<T>
 {
   public T Id { get; set; }
